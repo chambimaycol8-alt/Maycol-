5022dd579f42763d9edc5b2e7d15dbb038f46909
chambimaycol8-alt <chambimaycol8@gmail.com>
2026-09-14 12:26:48 -0500

    Add files via upload

---
+++ ejercio4 .cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n; // Lee el número ingresado por el usuario
+    
+    if (n % 2 == 0) {
+        cout << "Par";   // Si el residuo es 0, es par
+    } else {
+        cout << "Impar"; // De lo contrario, es impar
+    }
+    
+    return 0;
+}
