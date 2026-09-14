6b812c5a57b2c340d2a9cb1d071bae10c7649a67
chambimaycol8-alt <chambimaycol8@gmail.com>
2026-09-14 14:41:15 -0500

    Remove comments from ejercicio5.cpp

--- ejercicio5 .cpp
+++ ejercicio5 .cpp
@@ -4,14 +4,11 @@ using namespace std;
 int main() {
     char C1, C2;
     
-    // Lectura de los dos caracteres
     cin >> C1 >> C2;
     
-    // Condición: si son iguales
     if (C1 == C2) {
         cout << "Iguales";
     } 
-    // Si no son iguales
     else {
         cout << "Diferentes";
     }
