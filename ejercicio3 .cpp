30245b8bc5ece52e254da099252bbf538fd64123
chambimaycol8-alt <chambimaycol8@gmail.com>
2026-09-14 12:25:36 -0500

    Add files via upload

---
+++ ejercicio3 .cpp
@@ -0,0 +1,9 @@
+if (N > 0) {
+    cout << "#+"; // Imprime si es positivo
+} else {
+    cout << "#-"; // Imprime si es negativo (o cero provisionalmente)
+}
+
+if (N == 0) {
+    cout << "#0"; // Imprime si es exactamente cero
+}
