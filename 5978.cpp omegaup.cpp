6ca170a95893ab63fa2b6d1f74b1236970a9309e
chambimaycol8-alt <chambimaycol8@gmail.com>
2026-09-14 23:05:50 -0500

    Add files via upload

---
+++ 5978.cpp omegaup.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string nombre1, nombre2;
+    long long a, b;
+    cin >> nombre1 >> a;
+    cin >> nombre2 >> b;
+    if (a > b)
+        cout << nombre1 << endl;
+    else
+        cout << nombre2 << endl;
+    return 0;
+}
